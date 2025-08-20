4d6fe93ad0ff0b3004b2f79061ebfd602b9be2ca
Marin Alquinga Josue Isaac <jimarin@espe.edu.ec>
2025-08-20 01:58:16 -0500

    frontend update url back

--- holistica-frontend/src/utils/apiClient.js
+++ holistica-frontend/src/utils/apiClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'https://two02550-pruebas-software-grupo4.onrender.com/api';
 
 class ApiClient {
   constructor(baseEndpoint) {
