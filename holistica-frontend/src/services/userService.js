eda7560f33e7a280f23a83775b87e6a123883959
Marin Alquinga Josue Isaac <jimarin@espe.edu.ec>
2025-08-20 02:01:14 -0500

    deploy update

--- holistica-frontend/src/services/userService.js
+++ holistica-frontend/src/services/userService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://two02550-pruebas-software-grupo4.onrender.com/api';
 
 // Configurar axios con el token de autorización
 const api = axios.create({
